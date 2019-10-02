789a18f6c471079bc144497602ba7f240a50b764
Philipp Flenker <hello@philippflenker.com>
2019-10-02 17:25:25 +0200

    into-raw-mode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
