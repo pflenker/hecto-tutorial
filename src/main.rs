e8b3bbb39debe154f8f6c45fb344ac9b88cbbb6b
Philipp Flenker <hello@philippflenker.com>
2019-10-05 14:56:11 +0200

    die-on-input-error

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,19 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
