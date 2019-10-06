4406b23a53b5d100eafd727e358fedd719d87c5a
Philipp Flenker <hello@philippflenker.com>
2019-10-06 14:34:02 +0200

    editor-run-implementation

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,33 @@
-pub struct Editor {
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!(e);
 }
