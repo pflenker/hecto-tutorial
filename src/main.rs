fce2152868d0304c38bf3f77677b745d00d32cf8
Philipp Flenker <hello@philippflenker.com>
2019-10-06 15:01:26 +0200

    use-editor-struct

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
 
-fn die(e: std::io::Error) {
-    panic!(e);
-}
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
 }
