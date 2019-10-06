8dede4133489af08fb37095fb6471533a15e72f5
Philipp Flenker <hello@philippflenker.com>
2019-10-06 13:33:40 +0200

    use-termion-keys

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
 
 fn die(e: std::io::Error) {
     panic!(e);
@@ -13,19 +10,19 @@ fn die(e: std::io::Error) {
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
                 }
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => die(err),
         }
     }
