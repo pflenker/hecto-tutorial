aff067a70ab23bc6021b9bc2231c162b8ac4feda
Philipp Flenker <hello@philippflenker.com>
2019-10-06 21:30:31 +0200

    reading-evaluating

--- src/editor.rs
+++ src/editor.rs
@@ -9,25 +9,30 @@ impl Editor {
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
         }
     }
     pub fn default() -> Self {
         Self {}
+    }
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
+    }
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
     }
 }
 
