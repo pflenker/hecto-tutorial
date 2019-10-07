60337f2e690c5cb96d90e3bac631602659d1da53
Philipp Flenker <hello@philippflenker.com>
2019-10-07 11:12:41 +0200

    show-welcome-message

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
@@ -49,9 +51,14 @@ impl Editor {
         Ok(())
     }
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height - 1 {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("~\r");
+            if row == height / 3 {
+                println!("Hecto editor -- version {}\r", VERSION)
+            } else {
+                println!("~\r");
+            }
         }
     }
 }
