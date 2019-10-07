9a47bbb05ab3c4d4a7b191270650fe6c93f784cf
Philipp Flenker <hello@philippflenker.com>
2019-10-07 10:33:31 +0200

    more-terminal-functions

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,5 @@
 use crate::Terminal;
-use std::io::{self, stdout, Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 pub struct Editor {
     should_quit: bool,
@@ -11,8 +8,6 @@ pub struct Editor {
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
@@ -33,17 +28,18 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -57,15 +53,7 @@ impl Editor {
     }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 fn die(e: std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!(e);
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,15 @@
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
 pub struct Terminal {
     size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
 }
 
 impl Terminal {
@@ -14,9 +20,28 @@ impl Terminal {
                 width: size.0,
                 height: size.1,
             },
+            _stdout: stdout().into_raw_mode()?,
         })
     }
     pub fn size(&self) -> &Size {
         &self.size
+    }
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+    pub fn cursor_position(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
     }
 }
