b5ebf82bd761b9392f88c2bfb1177981f414c270
Philipp Flenker <hello@philippflenker.com>
2019-10-07 14:40:54 +0200

    open-file-from-params

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use crate::Document;
 use crate::Row;
 use crate::Terminal;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -33,10 +34,18 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            document: Document::default(),
+            document,
             cursor_position: Position::default(),
         }
     }
