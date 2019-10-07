7f3dcf10f7593ffb8692adcbbe8c06b93750675e
Philipp Flenker <hello@philippflenker.com>
2019-10-07 17:07:41 +0200

    right-at-line-end

--- src/editor.rs
+++ src/editor.rs
@@ -131,7 +131,10 @@ impl Editor {
             }
             Key::Right => {
                 if x < width {
-                    x = x.saturating_add(1);
+                    x += 1;
+                } else if y < height {
+                    y += 1;
+                    x = 0;
                 }
             }
             Key::PageUp => {
