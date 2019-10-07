d3c2f757fe7d052ae7955dfb7f881f1874f3cfac
Philipp Flenker <hello@philippflenker.com>
2019-10-07 11:31:03 +0200

    center-welcome-message

--- src/editor.rs
+++ src/editor.rs
@@ -50,14 +50,22 @@ impl Editor {
         }
         Ok(())
     }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
         for row in 0..height - 1 {
             Terminal::clear_current_line();
             if row == height / 3 {
-                let welcome_message = format!("Hecto editor -- version {}", VERSION);
-                let width = std::cmp::min(self.terminal.size().width as usize, welcome_message.len());                
-                println!("{}\r", &welcome_message[..width])
+                self.draw_welcome_message();
             } else {
                 println!("~\r");
             }
