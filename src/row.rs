af87b41ba567a9bf6232f37cdee255ef4fc28d99
Philipp Flenker <hello@philippflenker.com>
2019-10-08 09:04:41 +0200

    use-unicode-segmentation

--- src/row.rs
+++ src/row.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub struct Row {
     string: String,
@@ -16,10 +17,18 @@ impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
-        self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            result.push_str(grapheme);
+        }
+        result
     }
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.string[..].graphemes(true).count()
     }
     pub fn is_empty(&self) -> bool {
         self.string.is_empty()
