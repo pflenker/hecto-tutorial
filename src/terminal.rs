63666d65d25e4d2ba2f1c982d0fab350b708d7ca
Philipp Flenker <hello@philippflenker.com>
2019-10-08 10:38:15 +0200

    uuuuuuuuunused

--- src/terminal.rs
+++ src/terminal.rs
@@ -34,7 +34,7 @@ impl Terminal {
 
     #[allow(clippy::cast_possible_truncation)]
     pub fn cursor_position(position: &Position) {
-        let Position{mut x, mut y} = position;
+        let Position { mut x, mut y } = position;
         x = x.saturating_add(1);
         y = y.saturating_add(1);
         let x = x as u16;
