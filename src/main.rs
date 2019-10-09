f0ee2139c055a83548a6c7e126e633b7157afee3
Philipp Flenker <hello@philippflenker.com>
2019-10-09 10:00:22 +0200

    sensible-clippy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 #![warn(clippy::all, clippy::pedantic, clippy::restriction)]
-#![allow(clippy::missing_docs_in_private_items)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::shadow_reuse,
+    clippy::print_stdout,
+    clippy::wildcard_enum_match_arm,
+    clippy::else_if_without_else
+)]
 mod document;
 mod editor;
 mod row;
