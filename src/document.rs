2f5a2af64d62d82b996a8f80f30cba13631d9000
Philipp Flenker <hello@philippflenker.com>
2019-10-09 11:07:57 +0200

    indexing-panic-1

--- src/document.rs
+++ src/document.rs
@@ -33,19 +33,20 @@ impl Document {
         self.rows.len()
     }
     fn insert_newline(&mut self, at: &Position) {
-        if at.y > self.len() {
+        if at.y > self.rows.len() {
             return;
         }
-        if at.y == self.len() {
+        if at.y == self.rows.len() {
             self.rows.push(Row::default());
             return;
         }
-        let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
+        #[allow(clippy::indexing_slicing)]
+        let new_row = self.rows[at.y].split(at.x);
         #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
     pub fn insert(&mut self, at: &Position, c: char) {
-        if at.y > self.len() {
+        if at.y > self.rows.len() {
             return;
         }
         self.dirty = true;
@@ -53,28 +54,29 @@ impl Document {
             self.insert_newline(at);
             return;
         }
-        if at.y == self.len() {
+        if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
             self.rows.push(row);
         } else {
-            let row = self.rows.get_mut(at.y).unwrap();
+            #[allow(clippy::indexing_slicing)]
+            let row = &mut self.rows[at.y];
             row.insert(at.x, c);
         }
     }
-    #[allow(clippy::integer_arithmetic)]
+    #[allow(clippy::integer_arithmetic, clippy::indexing_slicing)]
     pub fn delete(&mut self, at: &Position) {
-        let len = self.len();
+        let len = self.rows.len();
         if at.y >= len {
             return;
         }
         self.dirty = true;
-        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y + 1< len {
+        if at.x == self.rows[at.y].len() && at.y + 1 < len {
             let next_row = self.rows.remove(at.y + 1);
-            let row = self.rows.get_mut(at.y).unwrap();
+            let row = &mut self.rows[at.y];
             row.append(&next_row);
         } else {
-            let row = self.rows.get_mut(at.y).unwrap();
+            let row = &mut self.rows[at.y];
             row.delete(at.x);
         }
     }
