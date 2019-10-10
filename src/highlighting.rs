1e29b17cc6241099eedc02719636792d08d9d841
Philipp Flenker <hello@philippflenker.com>
2019-10-10 08:58:55 +0200

    add-match-type

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -3,12 +3,14 @@ use termion::color;
 pub enum Type {
     None,
     Number,
+    Match,
 }
 
 impl Type {
     pub fn to_color(&self) -> impl color::Color {
         match self {
             Type::Number => color::Rgb(220, 163, 163),
+            Type::Match => color::Rgb(38, 139, 210),
             _ => color::Rgb(255, 255, 255),
         }
     }
