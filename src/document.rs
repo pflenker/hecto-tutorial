e34176fffbccee47ae09d5bf7924e7fc512f50b5
Philipp Flenker <hello@philippflenker.com>
2019-11-08 09:52:17 +0100

    multiline-comments

--- src/document.rs
+++ src/document.rs
@@ -20,7 +20,7 @@ impl Document {
         let mut rows = Vec::new();
         for value in contents.lines() {
             let mut row = Row::from(value);
-            row.highlight(&file_type.highlighting_options(), None);
+            row.highlight(&file_type.highlighting_options(), None, false);
             rows.push(row);
         }
         Ok(Self {
@@ -53,8 +53,8 @@ impl Document {
         #[allow(clippy::indexing_slicing)]
         let current_row = &mut self.rows[at.y];
         let mut new_row = current_row.split(at.x);
-        current_row.highlight(&self.file_type.highlighting_options(), None);
-        new_row.highlight(&self.file_type.highlighting_options(), None);
+        current_row.highlight(&self.file_type.highlighting_options(), None, false);
+        new_row.highlight(&self.file_type.highlighting_options(), None, false);
         #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
@@ -70,13 +70,13 @@ impl Document {
         if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
-            row.highlight(&self.file_type.highlighting_options(), None);
+            row.highlight(&self.file_type.highlighting_options(), None, false);
             self.rows.push(row);
         } else {
             #[allow(clippy::indexing_slicing)]
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);
-            row.highlight(&self.file_type.highlighting_options(), None);
+            row.highlight(&self.file_type.highlighting_options(), None, false);
         }
     }
     #[allow(clippy::integer_arithmetic, clippy::indexing_slicing)]
@@ -90,21 +90,26 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
-            row.highlight(&self.file_type.highlighting_options(), None);
+            row.highlight(&self.file_type.highlighting_options(), None, false);
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
-            row.highlight(&self.file_type.highlighting_options(), None);
+            row.highlight(&self.file_type.highlighting_options(), None, false);
         }
     }
     pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
             let mut file = fs::File::create(file_name)?;
             self.file_type = FileType::from(file_name);
+            let mut start_with_comment = false;
             for row in &mut self.rows {
                 file.write_all(row.as_bytes())?;
                 file.write_all(b"\n")?;
-                row.highlight(&self.file_type.highlighting_options(), None)
+                start_with_comment = row.highlight(
+                    &self.file_type.highlighting_options(),
+                    None,
+                    start_with_comment,
+                );
             }
             self.dirty = false;
         }
@@ -150,8 +155,13 @@ impl Document {
         None
     }
     pub fn highlight(&mut self, word: Option<&str>) {
+        let mut start_with_comment = false;
         for row in &mut self.rows {
-            row.highlight(&self.file_type.highlighting_options(), word);
+            start_with_comment = row.highlight(
+                &self.file_type.highlighting_options(),
+                word,
+                start_with_comment,
+            );
         }
     }
 }
--- src/row.rs
+++ src/row.rs
@@ -399,14 +399,36 @@ impl Row {
         }
         false
     }
-    pub fn highlight(&mut self, opts: &HighlightingOptions, word: Option<&str>) {
+    #[allow(clippy::indexing_slicing, clippy::integer_arithmetic)]
+    pub fn highlight(
+        &mut self,
+        opts: &HighlightingOptions,
+        word: Option<&str>,
+        start_with_comment: bool,
+    ) -> bool {
         self.highlighting = Vec::new();
         let chars: Vec<char> = self.string.chars().collect();
         let mut index = 0;
+        let mut in_ml_comment = start_with_comment;
+        if in_ml_comment {
+            let closing_index = if let Some(closing_index) = self.string.find("*/") {
+                closing_index + 2
+            } else {
+                chars.len()
+            };
+            for _ in 0..closing_index {
+                self.highlighting.push(highlighting::Type::MultilineComment);
+            }
+            index = closing_index;
+        }
         while let Some(c) = chars.get(index) {
+            if self.highlight_multiline_comment(&mut index, &opts, *c, &chars) {
+                in_ml_comment = true;
+                continue;
+            }
+            in_ml_comment = false;
             if self.highlight_char(&mut index, opts, *c, &chars)
                 || self.highlight_comment(&mut index, opts, *c, &chars)
-                || self.highlight_multiline_comment(&mut index, &opts, *c, &chars)
                 || self.highlight_primary_keywords(&mut index, &opts, &chars)
                 || self.highlight_secondary_keywords(&mut index, &opts, &chars)
                 || self.highlight_string(&mut index, opts, *c, &chars)
@@ -418,6 +440,10 @@ impl Row {
             index += 1;
         }
         self.highlight_match(word);
+        if in_ml_comment && &self.string[self.string.len().saturating_sub(2)..] != "*/" {
+            return true;
+        }
+        false
     }
 }
 
